a4f064f13b380d23a7ed2b18e8b1b6c5b864ef3f
Balsa <balsa.smi15@gmail.com>
2021-01-19 00:18:25 +0100

    Change urls and config not to include auth

--- uks/auth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
-]
