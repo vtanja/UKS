8ce2f78e3c74e2e2983a122cee44e18817eb58eb
Balsa <balsa.smi15@gmail.com>
2021-01-19 16:35:13 +0100

    Create welcome page

--- uks/security/urls.py
+++ uks/security/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='security/login.html'), name='login')
+    path('', views.welcome, name='welcome'),
 ]
