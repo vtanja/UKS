5dc050603286dcfb3b38d7c8787630387a39b380
Balsa <balsa.smi15@gmail.com>
2021-01-19 16:35:34 +0100

    Update login page

--- uks/security/urls.py
+++ uks/security/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from . import views
+
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('login/', auth_views.LoginView.as_view(template_name='security/login.html'), name='login'),
 ]
