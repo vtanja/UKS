e762526ec01c3663e8acf4c6f0b4df8863c45948
vtanja <51333448+vtanja@users.noreply.github.com>
2021-01-24 20:34:02 +0100

    List of user repositories preview implemented

--- uks/app/dashboard/views.py
+++ uks/app/dashboard/views.py
@@ -1,11 +1,15 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
+from security.models import SiteUser
 # Create your views here.
 
 
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    repositories = request.user.siteuser.repositories.all()
+    context = {'repositories': repositories}
+    return render(request, 'dashboard/dashboard.html', context)
+
 
 def profile(request):
     return render(request, 'dashboard/profile.html')
