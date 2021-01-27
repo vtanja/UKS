a2832164b2d74cb55118105f3d3a749ccce3a672
Tanja <tanja.vukmirovic8@gmail.com>
2021-01-27 18:17:02 +0100

    Password reset implemented

--- uks/app/user/urls.py
+++ uks/app/user/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('password-reset', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'),
                         name='password-reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),
+                        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),
+                        name='password_reset_confirm'),
+path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'),
+                        name='password_reset_complete'),
 ]
