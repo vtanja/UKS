9d3ce250b122337151b8bde4f071d4c653b21b23
Tanja <tanja.vukmirovic8@gmail.com>
2021-01-27 18:16:48 +0100

    Email configured

--- uks/uks/settings.py
+++ uks/uks/settings.py
@@ -149,3 +149,10 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ukstim3@gmail.com'
+EMAIL_HOST_PASSWORD = 'asdf1234.'
