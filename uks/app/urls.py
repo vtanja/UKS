b260c9e7b535e3de2816c130bc6b8c6521141d3b
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-01-28 15:45:56 +0100

    Point the root url configuration at the milestone urls module

--- uks/app/urls.py
+++ uks/app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('app.user.urls')),
     path('user/', include('app.user.urls')),
+    path('milestone/', include('app.milestone.urls')),
 ]
