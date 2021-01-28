2ec09a6cb70dd44c708fb8d445f5c76ab841ea31
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-01-28 15:47:19 +0100

    Add Milestone model definition

--- uks/app/milestone/models.py
+++ uks/app/milestone/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Milestone(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    dateCreated = models.DateField()
+    dueDate = models.DateField()
