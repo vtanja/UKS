6a78f53c58c2c223109b25e5034816fa46b61e3f
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-01-28 18:58:11 +0100

    Add repository foreign key to milestone

--- uks/app/milestone/models.py
+++ uks/app/milestone/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+from app.user.models import Repository
+
+
 class Milestone(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
     dateCreated = models.DateField()
     dueDate = models.DateField()
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
