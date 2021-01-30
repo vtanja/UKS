aa13e0dab8a6585ad4d871e9055d6ce2aa1c455c
Balsa <balsa.smi15@gmail.com>
2021-01-30 21:31:42 +0100

    Create Issue model

--- uks/apps/issue/models.py
+++ uks/apps/issue/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Issue(models.Model):
+
+    class IssueStatus(models.TextChoices):
+        TODO = 'TODO', _('To do')
+        ONGOING = 'ONGOING', _('Ongoing')
+        DONE =  'DONE', _('Done')
+
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    issue_status = models.CharField(
+        max_length=8,
+        choices=IssueStatus.choices,
+        default=IssueStatus.TODO
+    )
