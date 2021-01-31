26c29791c28e246f76c843ed16ab057389917f3f
Balsa <balsa.smi15@gmail.com>
2021-02-01 00:38:28 +0100

    Create link for repository issues

--- uks/apps/repository/urls.py
+++ uks/apps/repository/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from apps.repository import views
 
 urlpatterns = [
     path('<int:id>/', views.detail, name='detail'),
+    path('<int:id>/issues/', include('apps.issue.urls')),
 ]
