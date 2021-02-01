b3d67976098a18f7464943e917e28e4e684ac1ee
Balsa <balsa.smi15@gmail.com>
2021-02-01 16:31:13 +0100

    Fix link to issue details

--- uks/apps/issue/urls.py
+++ uks/apps/issue/urls.py
@@ -3,5 +3,5 @@ from .views import IssuesListView, IssueDetailView
 
 urlpatterns = [
     path('', IssuesListView.as_view(), name='repository_issues'),
-    path('<int:pk>/', IssueDetailView.as_view(), name='issue-details'),
+    path('<int:pk>/', IssueDetailView.as_view(), name='issue_details'),
 ]
