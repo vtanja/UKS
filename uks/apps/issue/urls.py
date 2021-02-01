4390abbdbdb67a2e954dacebb178ebab223a2b69
Balsa <balsa.smi15@gmail.com>
2021-02-02 00:34:40 +0100

    Create button and url for creating issues

--- uks/apps/issue/urls.py
+++ uks/apps/issue/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IssuesListView, IssueDetailView
+from .views import IssuesListView, IssueDetailView, CreateIssueView
 
 urlpatterns = [
     path('', IssuesListView.as_view(), name='repository_issues'),
     path('<int:pk>/', IssueDetailView.as_view(), name='issue_details'),
+    path('add/', CreateIssueView.as_view(), name='issue_add'),
 ]
