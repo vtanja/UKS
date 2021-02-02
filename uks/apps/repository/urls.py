fd45526c56de919a2621ce72122041c882075c68
Balsa <balsa.smi15@gmail.com>
2021-02-02 22:13:32 +0100

    Change repository detail structure

--- uks/apps/repository/urls.py
+++ uks/apps/repository/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.repository import views
 
 urlpatterns = [
-    path('detail/?P<int:id>/', views.detail, name='detail'),
+    path('<int:id>/', views.detail, name='detail'),
 ]
