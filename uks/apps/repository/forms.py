3acebdb992fe03e4fb7f3014de017514bf143700
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-03 12:21:38 +0100

    Adding repo url while creating new repository

--- uks/apps/repository/forms.py
+++ uks/apps/repository/forms.py
@@ -4,12 +4,7 @@ from security.models import SiteUser
 from .models import Repository
 
 
-class ProfileImageUpdateForm(forms.ModelForm):
-    class Meta:
-        model = SiteUser
-        fields = ['profile_img']
-
 class RepositoryForm(forms.ModelForm):
     class Meta:
         model = Repository
-        fields = ['name', 'description']
+        fields = ['name', 'description', 'repo_url']
