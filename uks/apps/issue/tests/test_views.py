0f57413a4d1ec078109e54087030ccb1ef59884a
Balsa <balsa.smi15@gmail.com>
2021-02-03 20:37:18 +0100

    Moved database creation to function
    Function will be called from multiple classes since they will have the same testing db

--- uks/apps/issue/tests/test_views.py
+++ uks/apps/issue/tests/test_views.py
@@ -8,53 +8,70 @@ from ...repository.models import Repository
 from ..models import Issue
 
 
+def fill_test_db():
+    test_user = User.objects.create_user(username='testuser', password='1E4@DAc#a1p')
+    test_user1 = User.objects.create_user(username='testuser1', password='4XC%4@1LSp')
+    test_user.save()
+    test_user1.save()
+
+    test_repository = Repository.objects.create(name='test_repository', description='test')
+    test_repository1 = Repository.objects.create(name='test_repository1', description='test2')
+    test_repository2 = Repository.objects.create(name='test_repository2', description='desc')
+    test_repository.save()
+    test_repository1.save()
+    test_repository2.save()
+
+    test_site_user = SiteUser.objects.create(user=test_user)
+    test_site_user1 = SiteUser.objects.create(user=test_user1)
+    test_site_user.repositories.add(test_repository, test_repository1)
+    test_site_user1.repositories.add(test_repository1, test_repository2)
+    test_site_user.save()
+    test_site_user1.save()
+
+    test_issue1 = Issue.objects.create(title='test issue', description='test', repository=test_repository,
+                                       created_by=test_user)
+    test_issue2 = Issue.objects.create(title='test issue', description='test desc', repository=test_repository,
+                                       created_by=test_user)
+    test_issue3 = Issue.objects.create(title='test issue 3', description='test', repository=test_repository1,
+                                       created_by=test_user1)
+    test_issue1.save()
+    test_issue2.save()
+    test_issue3.save()
+
+
 class IssueListViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        test_user = User.objects.create_user(username='testuser', password='1E4@DAc#a1p')
-        test_user1 = User.objects.create_user(username='testuser1', password='4XC%4@1LSp')
-        test_user.save()
-        test_user1.save()
-
-        test_repository = Repository.objects.create(name='test_repository', description='test')
-        test_repository1 = Repository.objects.create(name='test_repository1', description='test2')
-
-        test_site_user = SiteUser.objects.create(user=test_user)
-        test_site_user.repositories.add(test_repository)
-        test_site_user.repositories.add(test_repository1)
-
-        test_issue1 = Issue.objects.create(title='test issue', description='test', repository=test_repository, created_by=test_user)
-        test_issue2 = Issue.objects.create(title='test issue', description='test desc', repository=test_repository, created_by=test_user)
-        test_issue3 = Issue.objects.create(title='test issue 3', description='test', repository=test_repository, created_by=test_user1)
-        test_issue1.save()
-        test_issue2.save()
-        test_issue3.save()
+        fill_test_db()
 
     def test_view_url_exists_at_desired_location(self):
-        repository = Repository.objects.get(id=1)
+        repository = Repository.objects.all()[0]
         response = self.client.get('/repository/{}/issues/'.format(repository.id))
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
-        repository = Repository.objects.get(id=1)
+        repository = Repository.objects.all()[0]
         response = self.client.get(reverse('repository_issues', kwargs={'id': repository.id}))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        repository = Repository.objects.get(id=1)
+        repository = Repository.objects.all()[0]
         response = self.client.get(reverse('repository_issues', kwargs={'id': repository.id}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'issue/issue_list.html')
 
     def test_repository_with_no_issues(self):
-        repository = Repository.objects.get(id=2)
+        repository = Repository.objects.all()[2]
         response = self.client.get(reverse('repository_issues', kwargs={'id': repository.id}))
         self.assertEqual(response.status_code, 200)
         self.assertTrue(len(response.context['object_list']) == 0)
 
-    def test_repository_with_issues(self):
-        repository = Repository.objects.get(id=1)
+    def test_repository_with_issues_added_to_it(self):
+        repository = Repository.objects.all()[0]
+        response = self.client.get(reverse('repository_issues', kwargs={'id': repository.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.context['object_list']) != 0)
         response = self.client.get(reverse('repository_issues', kwargs={'id': repository.id}))
         self.assertEqual(response.status_code, 200)
         self.assertTrue(len(response.context['object_list']) == 3)
