0d34f0ff1843e9acd998dd899f77ceae43a81e14
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-03 12:21:01 +0100

    Added url field to repository model

--- uks/apps/repository/models.py
+++ uks/apps/repository/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Repository(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
+    repo_url = models.CharField(max_length=100, default='https://github.com/vtanja/UKS')
