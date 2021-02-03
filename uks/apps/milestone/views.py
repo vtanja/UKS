ac84718d2e845b119019d656db69edc91f82dbef
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-03 18:49:56 +0100

    Create ListView for milestones

--- uks/apps/milestone/views.py
+++ uks/apps/milestone/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 # Create your views here.
 from django.http import HttpResponse
 
+from apps.milestone.models import Milestone
+from apps.repository.models import Repository
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the milestones index page.")
+
+
+class MilestoneListView(ListView):
+    model = Milestone
+    template_name = 'milestone/milestone_list.html'
+
+    def get_queryset(self):
+        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        return Milestone.objects.filter(repository=self.repository)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MilestoneListView, self).get_context_data(**kwargs)
+        context['repository'] = self.repository
+        return context
