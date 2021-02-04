fe0443392cd1c75927be1452f12347a6717083b8
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-04 20:00:22 +0100

    Added paths for repository detail and  adding new reposotory
    Adding new repository moved from previous location

--- uks/apps/repository/urls.py
+++ uks/apps/repository/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from apps.repository import views
+from apps.repository.views import RepositoryDetailView
 
 urlpatterns = [
-    path('<int:id>/', views.detail, name='detail'),
+    path('<int:pk>/', RepositoryDetailView.as_view(), name='detail'),
+    # path('<int:pk>/overview', RepositoryDetailView.as_view(), name='overview'),
+    path('add', views.addRepository, name='add'),
+    path('<int:pk>/branch/', include('apps.branch.urls')),
 ]
