84abe4835f6ca3f916a31ffff71b3f08f50cf8f5
Balsa <balsa.smi15@gmail.com>
2021-02-04 23:28:46 +0100

    Remove hard-coded content from create issue tests

--- uks/apps/issue/tests/test_views.py
+++ uks/apps/issue/tests/test_views.py
@@ -152,35 +152,41 @@ class CreateIssueViewTest(TestCase):
         fill_test_db()
 
     def test_redirect_if_user_not_logged_in(self):
-        response = self.client.get('/repository/{}/issues/add/'.format(1))
-        self.assertRedirects(response, '/welcome/login/?next=/repository/1/issues/add/')
+        repository = Repository.objects.all()[0].id
+        response = self.client.get('/repository/{}/issues/add/'.format(repository))
+        self.assertRedirects(response, '/welcome/login/?next=/repository/{}/issues/add/'.format(repository))
 
     def test_logged_in_user_can_access(self):
-        login = self.client.login(username='testuser', password='1E4@DAc#a1p')
-        response = self.client.get(reverse('issue_add', kwargs={'id': 1}))
+        repository = Repository.objects.all()[0].id
+        self.client.login(username='testuser', password='1E4@DAc#a1p')
+        response = self.client.get(reverse('issue_add', kwargs={'id': repository}))
 
-        self.assertEqual(str(response.context['user']), 'testuser')
+        self.assertEqual(str(response.wsgi_request.user), 'testuser')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'issue/issue_form.html')
 
     def test_view_shows_correct_template(self):
-        login = self.client.login(username='testuser', password='1E4@DAc#a1p')
-        response = self.client.get(reverse('issue_add', kwargs={'id': 1}))
+        repository = Repository.objects.all()[0].id
+        self.client.login(username='testuser', password='1E4@DAc#a1p')
+        response = self.client.get(reverse('issue_add', kwargs={'id': repository}))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'issue/issue_form.html')
 
     def test_logged_in_user_adding_to_non_existent_repository(self):
-        login = self.client.login(username='testuser', password='1E4@DAc#a1p')
-        response = self.client.get(reverse('issue_add', kwargs={'id': 101}))
+        repositories = Repository.objects.all()
+        non_existent_id = repositories[len(repositories) - 1].id + 1
+        self.client.login(username='testuser', password='1E4@DAc#a1p')
+        response = self.client.get(reverse('issue_add', kwargs={'id': non_existent_id}))
 
         self.assertEqual(response.status_code, 404)
         self.assertRaisesMessage(Http404, 'No Repository matches given query.')
 
     def test_redirects_to_repository_issues_on_success(self):
-        login = self.client.login(username='testuser', password='1E4@DAc#a1p')
-        response = self.client.post(reverse('issue_add', kwargs={'id': 1}),
+        repository = Repository.objects.all()[0].id
+        self.client.login(username='testuser', password='1E4@DAc#a1p')
+        response = self.client.post(reverse('issue_add', kwargs={'id': repository}),
                                     {'title': 'Test issue', 'description': 'Test description'})
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/repository/1/issues/')
+        self.assertRedirects(response, '/repository/{}/issues/'.format(repository))
 
