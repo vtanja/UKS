9ba858330b7a30a320044ff3ee79aa09c99a8ca3
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-04 21:49:06 +0100

    Initial branch detail overview

--- uks/apps/branch/views.py
+++ uks/apps/branch/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.views.generic import DetailView
+
+from apps.branch.models import Branch
+
+
+class BranchDetailView(DetailView):
+    model = Branch
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        # self.repository = get_object_or_404(Repository, id=self.kwargs['pk'])
+        # self.branches = self.get_branches()
+
+        context = super(BranchDetailView, self).get_context_data(**kwargs)
+        # context['branches'] = self.branches
+        # context['first'] = self.branches[0]
+        return context
