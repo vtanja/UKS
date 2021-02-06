1f3564b2372478cd4dd6299c9fc9b4b13c68af15
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-06 20:11:17 +0100

    BranchListView implemented

--- uks/apps/branch/views.py
+++ uks/apps/branch/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+import json
+import os
+
+import requests
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-from django.views.generic import DetailView
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, DeleteView, UpdateView
 
+from apps.branch.forms import BranchForm
 from apps.branch.models import Branch
+from apps.repository.models import Repository
 
 
 class BranchDetailView(DetailView):
@@ -16,4 +25,17 @@ class BranchDetailView(DetailView):
         context = super(BranchDetailView, self).get_context_data(**kwargs)
         # context['branches'] = self.branches
         # context['first'] = self.branches[0]
+        return context
+
+
+class BranchListView(ListView):
+    model = Branch
+
+    def get_queryset(self):
+        self.repository = get_object_or_404(Repository, id=self.kwargs['pk'])
+        return Branch.objects.filter(repository=self.repository)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(BranchListView, self).get_context_data(**kwargs)
+        context['repository'] = self.repository
         return context
