15714858cb88e8a6791c89da46794795bcfd41a9
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-06 20:09:56 +0100

    Branch model update

--- uks/apps/branch/models.py
+++ uks/apps/branch/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 # Create your models here.
+from apps.repository.models import Repository
+
+
 class Branch(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+    def as_dict(self):
+        return {
+            "name": self.name
+        }
