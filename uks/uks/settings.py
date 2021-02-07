091a4f9e4b90bc7e556ef5b5cdf19471a0c3f660
Balsa <balsa.smi15@gmail.com>
2021-02-07 19:33:55 +0100

    Added allowed hosts to settings file

--- uks/uks/settings.py
+++ uks/uks/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG_MODE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
