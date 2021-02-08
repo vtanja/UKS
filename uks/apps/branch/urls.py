bbfb95e163f59d02fc53a27988ebe95928251ffb
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-08 19:37:45 +0100

    Added url for deleting branch

--- uks/apps/branch/urls.py
+++ uks/apps/branch/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.branch.views import BranchListView
+from apps.branch.views import BranchListView, BranchDeleteView
 
 urlpatterns = [
     path('', BranchListView.as_view(), name='branch_list'),
+    path('<int:pk>/delete/', BranchDeleteView.as_view(), name='branch_delete'),
 ]
