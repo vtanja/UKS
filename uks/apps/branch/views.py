871473b510465cd016e2ff5d52b92a1725122eb8
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-08 19:38:08 +0100

    Implemented BranchDeleteView

--- uks/apps/branch/views.py
+++ uks/apps/branch/views.py
@@ -4,8 +4,9 @@ import os
 import requests
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, DeleteView
 
 from apps.branch.models import Branch
 from apps.repository.models import Repository
@@ -28,10 +29,17 @@ class BranchListView(ListView):
     model = Branch
 
     def get_queryset(self):
-        self.repository = get_object_or_404(Repository, id=self.kwargs['pk'])
+        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
         return Branch.objects.filter(repository=self.repository)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(BranchListView, self).get_context_data(**kwargs)
         context['repository'] = self.repository
         return context
+
+
+class BranchDeleteView(DeleteView):
+    model = Branch
+
+    def get_success_url(self):
+        return reverse_lazy('branch_list', kwargs={'id': self.kwargs['id']})
