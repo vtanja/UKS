299bbd6cbe23ca8e1b85f3aa5bc5c7c55c45c9cd
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-09 12:50:10 +0100

    Added url for details view

--- uks/apps/milestone/urls.py
+++ uks/apps/milestone/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MilestoneListView, CreateMilestoneView
+from .views import MilestoneListView, CreateMilestoneView, MilestoneDetailView
 
 urlpatterns = [
     path('', MilestoneListView.as_view(), name='repository_milestones'),
     path('add/', CreateMilestoneView.as_view(), name='create_milestone'),
+    path('<int:pk>/', MilestoneDetailView.as_view(), name='milestone_details'),
 ]
