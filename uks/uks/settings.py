ac8588fd6372821faaa9cb0c1b3482e6e430a525
Balsa <balsa.smi15@gmail.com>
2021-02-09 13:22:11 +0100

    Change timezone and default datetime format

--- uks/uks/settings.py
+++ uks/uks/settings.py
@@ -133,13 +133,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Belgrade'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATETIME_FORMAT = 'd. N Y, H:i:s'
 
 
 # Static files (CSS, JavaScript, Images)
