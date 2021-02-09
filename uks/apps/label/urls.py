e68290bd91a97692c1181c245b913dc69ae11b00
veca22 <57525487+veca22@users.noreply.github.com>
2021-02-09 11:28:57 +0100

    New label
    Added function for adding a new label

--- uks/apps/label/urls.py
+++ uks/apps/label/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from .views import ListLabelView
+from .views import ListLabelView, CreateLabel
 
 urlpatterns = [
     path('', ListLabelView.as_view(), name='repository_labels'),
+    path('add/', CreateLabel.as_view(), name='create_label'),
 ]
--- uks/apps/label/views.py
+++ uks/apps/label/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-from apps.label.models import Label
+from .forms import CreateLabelForm
+from .models import Label
 from apps.repository.models import Repository
 
 
@@ -19,3 +22,27 @@ class ListLabelView(ListView):
         context = super(ListLabelView, self).get_context_data(**kwargs)
         context['repository'] = self.repository
         return context
+
+
+class CreateLabel(LoginRequiredMixin, CreateView):
+    model = Label
+    template_name = 'create_label.html'
+    form_class = CreateLabelForm
+
+    def form_valid(self, form):
+        form.instance.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(CreateLabel, self).get_form_kwargs()
+        kwargs['repository'] = get_object_or_404(Repository, id=self.kwargs['id'])
+        return kwargs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CreateLabel, self).get_context_data(**kwargs)
+        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        context['repository'] = self.repository
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('repository_labels', kwargs={'id': self.kwargs['id']})
