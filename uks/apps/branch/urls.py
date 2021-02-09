b36012e20bc75a2d5f72dfc941ed7dd2cbf98545
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-09 16:43:47 +0100

    Added url for updating branch

--- uks/apps/branch/urls.py
+++ uks/apps/branch/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
+from apps.branch import views
 from apps.branch.views import BranchListView, BranchDeleteView
 
 urlpatterns = [
     path('', BranchListView.as_view(), name='branch_list'),
     path('<int:pk>/delete/', BranchDeleteView.as_view(), name='branch_delete'),
+    path('<int:pk>/update/', views.update_branch, name='branch_update'),
 ]
