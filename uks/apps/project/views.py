8b3fe2e54a65630a0735fcf6d850395f22dc5344
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-10 21:47:54 +0100

    Detail view created and get method for changing issue status

--- uks/apps/project/views.py
+++ uks/apps/project/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from apps.project.models import Project
 from apps.repository.models import Repository
+from apps.issue.models import Issue
 
 
 class ProjectListView(ListView):
@@ -44,3 +46,24 @@ class CreateProjectView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse_lazy('repository_projects', kwargs={'id': self.kwargs['id']})
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'project/project_details.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProjectDetailView, self).get_context_data(**kwargs)
+        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        self.project = get_object_or_404(Project, id=self.kwargs['pk'])
+        context['repository'] = self.repository
+        context['issues'] = Issue.objects.filter(project=self.project)
+        return context
+
+
+def update_issue(self, id):
+    issue_id = self.GET.get('id')
+    list_id = self.GET.get('list_id')
+    issue = Issue.objects.filter(id=issue_id).first()
+    issue.change_status(list_id)
+    return HttpResponse()
