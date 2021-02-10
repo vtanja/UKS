0dd1abc41029b1d923a177d85d45a0d6beffa0aa
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-10 22:24:22 +0100

    Added project update path

--- uks/apps/project/urls.py
+++ uks/apps/project/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
-from .views import ProjectListView, CreateProjectView, ProjectDetailView
+from .views import ProjectListView, CreateProjectView, ProjectDetailView, ProjectUpdateView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='repository_projects'),
     path('add/', CreateProjectView.as_view(), name='create_project'),
     path('<int:pk>/', ProjectDetailView.as_view(), name='project_details'),
+    path('<int:pk>/update', ProjectUpdateView.as_view(), name='project_update'),
     path('ajax/update_issue/', views.update_issue, name='update_issue'),
 ]
 urlpatterns += staticfiles_urlpatterns()
