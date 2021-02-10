bdf881655c7b3c00050f4ee0f21db4fbc3a192ef
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-10 16:00:32 +0100

    Added path to CreateProjectView

--- uks/apps/project/urls.py
+++ uks/apps/project/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProjectListView
+from .views import ProjectListView, CreateProjectView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='repository_projects'),
+    path('add/', CreateProjectView.as_view(), name='create_project'),
 ]
