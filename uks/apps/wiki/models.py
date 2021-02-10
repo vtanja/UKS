ba60d78c2b82978e17d9c9ca84caac7b09af78a3
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-10 21:27:31 +0100

    Added initial wiki model

--- uks/apps/wiki/models.py
+++ uks/apps/wiki/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from apps.repository.models import Repository
+
+
+class Wiki(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE, null=False)
