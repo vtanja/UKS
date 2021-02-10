ca53705892a873aad1498886a95819615aa88f4e
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-10 15:22:16 +0100

    Created ListView for all projects in repository

--- uks/apps/project/views.py
+++ uks/apps/project/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.views.generic import ListView
+
+from apps.project.models import Project
+
+from apps.repository.models import Repository
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'project/project_list.html'
+
+    def get_queryset(self):
+        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        return Project.objects.filter(repository=self.repository)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProjectListView, self).get_context_data(**kwargs)
+        context['repository'] = self.repository
+        return context
