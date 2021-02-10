5d809c26764e074d6d85174b2f1b2e4ed485af62
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-10 21:48:20 +0100

    Add paths to detail view and get method

--- uks/apps/project/urls.py
+++ uks/apps/project/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from .views import ProjectListView, CreateProjectView
+from . import views
+from .views import ProjectListView, CreateProjectView, ProjectDetailView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='repository_projects'),
     path('add/', CreateProjectView.as_view(), name='create_project'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project_details'),
+    path('ajax/update_issue/', views.update_issue, name='update_issue'),
 ]
+urlpatterns += staticfiles_urlpatterns()
