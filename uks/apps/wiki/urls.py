efb37a19903cdbfb6b2f280d04ee044702b85dd5
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-11 16:17:18 +0100

    Added url for creating wiki page

--- uks/apps/wiki/urls.py
+++ uks/apps/wiki/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.wiki.views import WikiListView, WikiDetailPage
+from apps.wiki.views import WikiListView, WikiDetailPage, CreateWikiView
 
 urlpatterns = [
     path('', WikiListView.as_view(), name='wiki-overview'),
     path('<int:pk>/', WikiDetailPage.as_view(), name='wiki-details'),
+    path('add/', CreateWikiView.as_view(), name='wiki-add'),
 ]
