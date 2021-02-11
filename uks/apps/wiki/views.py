f3affd2b4f229d63474afb365277c629fa83275e
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-11 16:17:32 +0100

    Implemented view for creating new wiki page

--- uks/apps/wiki/views.py
+++ uks/apps/wiki/views.py
@@ -3,9 +3,11 @@ import logging
 from django.shortcuts import  get_object_or_404
 
 # Create your views here.
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from apps.repository.models import Repository
+from apps.wiki.forms import CreateWikiForm
 from apps.wiki.models import Wiki
 
 logger = logging.getLogger('django')
@@ -38,3 +40,31 @@ class WikiDetailPage(DetailView):
         context['wikis'] = Wiki.objects.filter(repository_id=self.kwargs['id'])
         context['repository'] = Repository.objects.get(id=self.kwargs['id'])
         return context
+
+
+class CreateWikiView(CreateView):
+    model = Wiki
+    form_class = CreateWikiForm
+    template_name = 'wiki/wiki_add.html'
+
+    def form_valid(self, form):
+        logger.info('Setting repository to wiki!')
+        form.instance.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        logger.info('Wiki page created!')
+        return super(CreateWikiView, self).form_valid(form)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        logger.info('Initializing context')
+        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        context = super(CreateWikiView, self).get_context_data(**kwargs)
+        context['repository'] = self.repository
+        logger.info('Context initialized!')
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super(CreateWikiView, self).get_form_kwargs()
+        kwargs['repository'] = get_object_or_404(Repository, id=self.kwargs['id'])
+        return kwargs
+
+    def get_success_url(self):
+        return reverse_lazy('wiki-overview', kwargs={'id': self.kwargs['id']})
