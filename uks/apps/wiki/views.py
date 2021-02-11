966bd1958e3feef326b35a035076257818ac3d9b
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-11 19:00:12 +0100

    Added editing view

--- uks/apps/wiki/views.py
+++ uks/apps/wiki/views.py
@@ -1,11 +1,12 @@
 import datetime
 import logging
 
-from django.shortcuts import  get_object_or_404
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from apps.repository.models import Repository
 from apps.user.models import UserHistoryItem
@@ -14,9 +15,16 @@ from apps.wiki.models import Wiki, WikiHistoryItem
 
 logger = logging.getLogger('django')
 
+
 class WikiListView(ListView):
     model = Wiki
     template_name = 'wiki/wiki_list.html'
+
+    def get(self, request, *args, **kwargs):
+        repo = Repository.objects.get(id=self.kwargs['id'])
+        if repo.wiki_set.count() > 0:
+            return redirect(reverse('wiki-details', kwargs={'id': repo.id, 'pk': repo.wiki_set.first().id}))
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
         logger.info('Getting current repository!')
@@ -85,3 +93,35 @@ class CreateWikiView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('wiki-overview', kwargs={'id': self.kwargs['id']})
+
+
+class WikiUpdateView(UpdateView):
+    model = Wiki
+    form_class = CreateWikiForm
+
+    def form_valid(self, form):
+        # Add issue change for actual changes
+        logger.log('Wiki page [%s] change initializes!', self.object.title)
+        response = super(WikiUpdateView, self).form_valid(form)
+        logger.log('Creating wiki history item!')
+        change = WikiHistoryItem()
+        change.belongsTo = self.request.user
+        change.message = 'changed wiki page'
+        change.dateChanged = datetime.datetime.now()
+        change.save()
+        logger.log('Wiki page [%s] change done!', self.object.title)
+        return response
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        context = super(WikiUpdateView, self).get_context_data(**kwargs)
+        context['repository'] = self.repository
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super(WikiUpdateView, self).get_form_kwargs()
+        kwargs['repository'] = get_object_or_404(Repository, id=self.kwargs['id'])
+        return kwargs
+
+    def get_success_url(self):
+        return reverse_lazy('wiki-details', kwargs={'id': self.kwargs['id'], 'pk': self.kwargs['pk']})
