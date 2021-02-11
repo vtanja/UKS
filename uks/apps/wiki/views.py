dab0b979b9283feb154d95cab9beaadace211b23
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-11 10:59:27 +0100

    WikiDetailView implemented

--- uks/apps/wiki/views.py
+++ uks/apps/wiki/views.py
@@ -3,7 +3,7 @@ import logging
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from apps.repository.models import Repository
 from apps.wiki.models import Wiki
@@ -12,7 +12,7 @@ logger = logging.getLogger('django')
 
 class WikiListView(ListView):
     model = Wiki
-    template_name = 'wiki/overview.html'
+    template_name = 'wiki/wiki_list.html'
 
     def get_queryset(self):
         logger.info('Getting current repository!')
@@ -20,8 +20,22 @@ class WikiListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(WikiListView, self).get_context_data(**kwargs)
+        wikis = Wiki.objects.filter(repository_id=self.repository.id)
         logger.info('Initializing context!')
         context['repository'] = self.repository
+        context['wikis'] = wikis
         # context['p_form'] = self.p_form
         context['show'] = False
+        return context
+
+
+class WikiDetailPage(DetailView):
+    model = Wiki
+    template_name = 'wiki/wiki_detail.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(WikiDetailPage, self).get_context_data(**kwargs)
+        logger.info('Retrieving wiki that belong to repository with id: %s', self.kwargs['id'])
+        context['wikis'] = Wiki.objects.filter(repository_id=self.kwargs['id'])
+        context['repository'] = Repository.objects.get(id=self.kwargs['id'])
         return context
