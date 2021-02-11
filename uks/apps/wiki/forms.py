ad206d999a478df05bdba1ceb8f30db090269394
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-11 16:16:55 +0100

    Added form for creating new wiki page

---
+++ uks/apps/wiki/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+from apps.wiki.models import Wiki
+
+
+class CreateWikiForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.repository = kwargs.pop('repository')
+        super(CreateWikiForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Wiki
+        fields = ['title', 'content']
+        # widgets = {
+        #     'content': forms.Edi(attrs={'type': 'date'})
+        # }
