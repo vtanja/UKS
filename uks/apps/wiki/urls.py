c8060277404772e9f2fc1a19a9d32cba1b1ccc35
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-11 19:00:25 +0100

    Added path for editing

--- uks/apps/wiki/urls.py
+++ uks/apps/wiki/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.wiki.views import WikiListView, WikiDetailPage, CreateWikiView
+from apps.wiki.views import WikiListView, WikiDetailPage, CreateWikiView, WikiUpdateView
 
 urlpatterns = [
     path('', WikiListView.as_view(), name='wiki-overview'),
     path('<int:pk>/', WikiDetailPage.as_view(), name='wiki-details'),
+    path('<int:pk>/edit/', WikiUpdateView.as_view(), name='wiki-update'),
     path('add/', CreateWikiView.as_view(), name='wiki-add'),
 ]
