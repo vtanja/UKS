494ace74d6ad027dc98bc76f7b60d5a88c36b368
Tanja <tanja.vukmirovic8@gmail.com>
2021-02-12 17:55:47 +0100

    Added user id to profile url

--- uks/apps/user/urls.py
+++ uks/apps/user/urls.py
@@ -7,7 +7,7 @@ from .views import AllIssuesListView
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('profile/', views.profile, name='profile'),
+    path('<int:pk>/profile/', views.profile, name='profile'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'),
          name='password-reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),
--- uks/security/models.py
+++ uks/security/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from apps.repository.models import Repository
 
@@ -9,3 +8,6 @@ from apps.repository.models import Repository
 class SiteUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_img = models.ImageField(default='profile_default.png')
+
+    def get_absolute_url(self):
+        return "/user/"+str(self.user.id) + "/profile/"
