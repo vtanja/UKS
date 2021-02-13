43cdcd7b515d0fbb6bec0b4edfc3738abf03a9c4
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-13 12:54:57 +0100

    Rename parameter from 'id' to 'repo_id' to avoid overshadowing of built in name

--- uks/apps/milestone/views.py
+++ uks/apps/milestone/views.py
@@ -22,7 +22,7 @@ class MilestoneListView(ListView):
     template_name = 'milestone/milestone_list.html'
 
     def get_queryset(self):
-        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        self.repository = get_object_or_404(Repository, id=self.kwargs['repo_id'])
         return Milestone.objects.filter(repository=self.repository)
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -38,22 +38,22 @@ class CreateMilestoneView(LoginRequiredMixin, CreateView):
     form_class = CreateMilestoneForm
 
     def form_valid(self, form):
-        form.instance.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        form.instance.repository = get_object_or_404(Repository, id=self.kwargs['repo_id'])
         return super().form_valid(form)
 
     def get_form_kwargs(self):
         kwargs = super(CreateMilestoneView, self).get_form_kwargs()
-        kwargs['repository'] = get_object_or_404(Repository, id=self.kwargs['id'])
+        kwargs['repository'] = get_object_or_404(Repository, id=self.kwargs['repo_id'])
         return kwargs
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(CreateMilestoneView, self).get_context_data(**kwargs)
-        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        self.repository = get_object_or_404(Repository, id=self.kwargs['repo_id'])
         context['repository'] = self.repository
         return context
 
     def get_success_url(self):
-        return reverse_lazy('repository_milestones', kwargs={'id': self.kwargs['id']})
+        return reverse_lazy('repository_milestones', kwargs={'repo_id': self.kwargs['repo_id']})
 
 
 class MilestoneDetailView(DetailView):
@@ -62,7 +62,7 @@ class MilestoneDetailView(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(MilestoneDetailView, self).get_context_data(**kwargs)
-        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        self.repository = get_object_or_404(Repository, id=self.kwargs['repo_id'])
         self.milestone = get_object_or_404(Milestone, id=self.kwargs['pk'])
         context['repository'] = self.repository
         context['issues'] = Issue.objects.filter(milestone=self.milestone)
@@ -76,18 +76,23 @@ class MilestoneUpdateView(UpdateView):
 
     def get_form_kwargs(self):
         kwargs = super(MilestoneUpdateView, self).get_form_kwargs()
-        kwargs['repository'] = get_object_or_404(Repository, id=self.kwargs['id'])
+        kwargs['repository'] = get_object_or_404(Repository, id=self.kwargs['repo_id'])
         return kwargs
 
     def get_context_data(self, **kwargs):
         context = super(MilestoneUpdateView, self).get_context_data(**kwargs)
-        self.repository = get_object_or_404(Repository, id=self.kwargs['id'])
+        self.repository = get_object_or_404(Repository, id=self.kwargs['repo_id'])
         self.milestone = get_object_or_404(Milestone, id=self.kwargs['pk'])
         context['repository'] = self.repository
         return context
 
     def get_success_url(self):
-        return reverse_lazy('repository_milestones', kwargs={'id': self.kwargs['id']})
+        return reverse_lazy('repository_milestones', kwargs={'repo_id': self.kwargs['repo_id']})
 
 
-
+@login_required
+def close_milestone(request, repo_id, pk):
+    milestone = get_object_or_404(Milestone, pk=pk)
+    repository = get_object_or_404(Repository, pk=repo_id)
+    milestone.toggle_milestone_close()
+    return redirect(reverse_lazy('repository_milestones', kwargs={'repo_id': repo_id}))
