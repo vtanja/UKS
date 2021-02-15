242275d648846321a29ae3da5716876f6bf6ff9b
Balsa <balsa.smi15@gmail.com>
2021-02-15 14:19:46 +0100

    Create Commit model class

--- uks/apps/commit/models.py
+++ uks/apps/commit/models.py
@@ -1,3 +1,17 @@
+from django.db import models
 
 
-# Create your models here.
+class Commit(models.Model):
+    url = models.URLField(verbose_name='URL to commit')
+    sha = models.TextField()
+    author = models.CharField(max_length=60)
+    date = models.DateTimeField()
+    message = models.TextField()
+    parents = models.ManyToManyField('self', symmetrical=False)
+
+    def __str__(self):
+        return '{author} committed {hash} at {date}'.format(author=self.author, hash=self.sha, date=self.date)
+
+    def get_absolute_url(self):
+        from django.shortcuts import redirect
+        return redirect(self.url)
