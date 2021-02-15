1283fd371bdc074ffe8dbc5ae0afbdd056619b87
Balsa <balsa.smi15@gmail.com>
2021-02-15 21:21:50 +0100

    Add absolute url to project and milestone

--- uks/apps/project/models.py
+++ uks/apps/project/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from apps.repository.models import Repository
+from django.urls import reverse
 
 
 class Project(models.Model):
@@ -11,3 +12,6 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('project_details', args=[str(self.repository_id), str(self.pk)])
