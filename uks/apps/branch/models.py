1273074acd9f839e434f28c5e1dde631535609a3
Balsa <balsa.smi15@gmail.com>
2021-02-15 14:20:19 +0100

    Add head commit to branch model

--- uks/apps/branch/models.py
+++ uks/apps/branch/models.py
@@ -1,13 +1,12 @@
-from django.db import models
-
-
-# Create your models here.
+from apps.commit.models import Commit
 from apps.repository.models import Repository
+from django.db import models
 
 
 class Branch(models.Model):
     name = models.CharField(max_length=100)
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    head = models.ForeignKey(Commit, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.name
