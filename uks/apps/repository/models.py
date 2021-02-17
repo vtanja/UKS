36b257f9f613effc1040bbc03b511f4cf0e3e7a5
Balsa <balsa.smi15@gmail.com>
2021-02-17 16:34:43 +0100

    Update repository name with unique and blank attrs

--- uks/apps/repository/models.py
+++ uks/apps/repository/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Repository(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(blank=True)
     repo_url = models.CharField(max_length=100, default='https://github.com/vtanja/UKS', blank=True, null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     collaborators = models.ManyToManyField(User, related_name='collaborators', blank=True)
