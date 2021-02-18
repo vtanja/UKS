b5b144d0414b67eab804fb57a3497664970c42c8
veca22 <57525487+veca22@users.noreply.github.com>
2021-02-18 17:50:03 +0100

    Update test_models.py

--- uks/apps/label/tests/test_models.py
+++ uks/apps/label/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.test import TestCase
 
 from ..models import Label
@@ -11,21 +12,21 @@ class LabelModelTest(TestCase):
         test_data_base()
 
     def test_label_name(self):
-        label = Label.objects.get(id=5)
+        label = get_object_or_404(Label, id=5)
         verbose_name = label._meta.get_field('name').verbose_name
         self.assertEquals(verbose_name, 'name')
 
     def test_label_name_max_length(self):
-        label = Label.objects.get(id=5)
+        label = get_object_or_404(Label, id=5)
         max_length = label._meta.get_field('name').max_length
         self.assertEquals(max_length, 30)
 
     def test_label_description_validators(self):
-        label = Label.objects.get(id=5)
+        label = get_object_or_404(Label, id=5)
         description_validators = label._meta.get_field('description').validators
         self.assertEquals(description_validators, [])
 
     def test_label_color_format(self):
-        label = Label.objects.get(id=5)
+        label = get_object_or_404(Label, id=5)
         color_format = label._meta.get_field('color').format
         self.assertEquals(color_format, 'hexa')
