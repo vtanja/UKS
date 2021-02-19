9bf053d13def604871146c9db46265cf934446b8
Balsa <balsa.smi15@gmail.com>
2021-02-19 23:47:14 +0100

    Change url parameter name from id to branch_id

--- uks/apps/branch/urls.py
+++ uks/apps/branch/urls.py
@@ -6,7 +6,7 @@ from apps.repository.views import RepositoryDetailView
 
 urlpatterns = [
     path('', BranchListView.as_view(), name='branch_list'),
-    path('<int:pk>/', RepositoryDetailView.as_view(), name='branch-detail'),
+    path('<int:branch_id>/', RepositoryDetailView.as_view(), name='branch-detail'),
     path('<int:pk>/delete/', BranchDeleteView.as_view(), name='branch_delete'),
     path('<int:pk>/update/', views.update_branch, name='branch_update'),
 ]
